9dd7d2fd1297879ac04c7f664566b03145725c49
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2025-03-26 14:58:21 -0400

    added some more stuff

--- app/src/main/java/com/example/notesappproject/MemoAdapter.java
+++ app/src/main/java/com/example/notesappproject/MemoAdapter.java
@@ -1,4 +1,69 @@
 package com.example.notesappproject;
 
-public class MemoAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MemoAdapter extends RecyclerView.Adapter {
+    private final ArrayList<Memo> memoData;
+    private boolean isDeleting;
+    private Context paraentContext;
+
+
+    private View.OnClickListener mOnItemClickListener;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MemoViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewTitle;
+        public TextView textDate;
+
+
+        public MemoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle = itemView.findViewById(R.id.textNoteTitle);
+            textDate = itemView.findViewById(R.id.textDatePublished);
+            itemView.setTag(this);
+            itemView.setOnClickListener(mOnItemClickListener);
+        }
+
+        public TextView getTextViewTitle() {
+            return textViewTitle;
+        }
+
+        public TextView getTextDate() {
+            return textDate;
+        }
+    }
+
+
+    public MemoAdapter(ArrayList<Memo> arrayList, Context context) {
+        memoData = arrayList;
+        paraentContext = context;
+    }
+
+    public void setOnItemClickListener(View.OnClickListener itemClickListener) {
+        mOnItemClickListener = itemClickListener;
+    }
 }
